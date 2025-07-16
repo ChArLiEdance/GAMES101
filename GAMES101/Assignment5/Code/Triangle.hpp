1b75f232c6bed4da6f440e1fa52c0af79bd061b4
Charlie <yuheng.23@intl.zju.edu.cn>
2025-07-16 15:41:45 +0800

    pdf

--- GAMES101/Assignment5/Code/Triangle.hpp
+++ GAMES101/Assignment5/Code/Triangle.hpp
@@ -11,6 +11,7 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    
     // Möller–Trumbore 算法
     const float EPSILON = 1e-8;
     Vector3f edge1 = v1 - v0;
